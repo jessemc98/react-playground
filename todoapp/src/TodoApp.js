c752fe2a6e90bd7c63d85512bc272d384d994153
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-09-26 20:32:17 +0200

    added createTodo method

--- todoapp/src/TodoApp.js
+++ todoapp/src/TodoApp.js
@@ -1,11 +1,32 @@
 import React, { Component} from 'react';
+import Todo from './Todo.js';
 
 class TodoApp extends Component{
+	constructor(){
+		super();
+		this.state = {
+			todos: []
+		};
+	}
+	componentWillMount() {
+		this.createTodo('get eggs', 1);
+	}
+
 	render() {
 		return (
-			<div>working!</div>
+			<ul>
+				{this.state.todos}
+			</ul>
 		);
 	}
+
+	createTodo(info, id){
+		const {todos} = this.state;
+		todos.push(<Todo key={id} info={info} />);
+		this.setState({todos});
+	}
+
+
 }
 
 export default TodoApp;
