c84921384af702cc017d9225c371dce57549ad0e
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-09-29 20:40:12 +0200

    broke out coursepage presentation into components

--- redux/my-redux/src/components/courses/CoursesPage.js
+++ redux/my-redux/src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as courseActions from '../../actions/courseActions'
+import CourseList from './CourseList'
 
 class CoursesPage extends Component {
 
@@ -10,10 +11,9 @@ class CoursesPage extends Component {
   }
 
   render() {
+    const { courses } = this.props
     return (
-      <div>
-        {this.props.courses.map(this._courseRow)}
-      </div>
+      <CourseList courses={courses} />
     )
   }
 }
