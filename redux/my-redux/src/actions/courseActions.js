62a423713fdaea3403ebe35b7c0758d33e21c077
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-09-29 20:33:17 +0200

    added thunk middleware to store and created loadCourse action which dispatches a LOAD_COURSES_SUCCESS action if successful

--- redux/my-redux/src/actions/courseActions.js
+++ redux/my-redux/src/actions/courseActions.js
@@ -1,5 +1,17 @@
 import * as types from './actionTypes'
+import courseApi from '../api/mockCourseApi.js'
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course}
+export function loadCoursesSuccess(courses) {
+  console.log('loaded');
+  return {type: types.LOAD_COURSES_SUCCESS, courses}
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses))
+    }).catch(err => {
+      throw(err)
+    })
+  }
 }
