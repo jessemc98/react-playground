ccca392fcfb73778369bded290298507ce7c8cf9
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-09-30 06:08:45 +0200

    author and course actions now dispatch beginAjaxCall actions

--- redux/my-redux/src/actions/authorActions.js
+++ redux/my-redux/src/actions/authorActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes'
 import authorApi from '../api/mockAuthorApi.js'
-
+import { beginAjaxCall } from './ajaxStatusActions.js'
 export function loadAuthorsSuccess(authors) {
 
   return {type: types.LOAD_AUTHORS_SUCCESS, authors}
@@ -8,6 +8,7 @@ export function loadAuthorsSuccess(authors) {
 
 export function loadAuthors() {
   return function(dispatch) {
+    dispatch(beginAjaxCall())
     return authorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorsSuccess(authors))
     }).catch(err => {
