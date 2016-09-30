d37e8b4d657d48ab8662c698b9ebc94060a9d351
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-09-30 06:02:30 +0200

    added ajaxCallsInProgress to the initialState

--- redux/my-redux/src/reducers/index.js
+++ redux/my-redux/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import authors from './authorReducer'
 import courses from './courseReducer'
+import ajaxCallsInProgres from './ajaxStatusReducer'
 
 const rootReducer = combineReducers({
   authors,
-  courses
+  courses,
+  ajaxCallsInProgres
 })
 
 export default rootReducer;
